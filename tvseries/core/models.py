9132e8aaee101d45c18cb8142dea77c2b1b13a2a
Andrei Maxwel Mülbauer <andreimaxwel@gmail.com>
2018-04-28 21:37:40 -0300

    Updated Model to Responses and Tags

--- tvseries/core/models.py
+++ tvseries/core/models.py
@@ -1,22 +1,52 @@
 from tvseries.ext import db
+import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Text
+from sqlalchemy.orm import relationship
 
 
-class TVSerie(db.Model):
+class Responses(db.Model):
     id = db.Column(db.Integer(),
                    nullable=False, unique=True,
                    autoincrement=True, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
+    end_user_id = db.Column(db.Integer, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text, nullable=True)
-    episodies_number = db.Column(db.Integer, nullable=False, default=1)
-    author = db.Column(db.String(50), nullable=False)
-    year = db.Column(db.Date)
+    campaign_id = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    ip_address = db.Column(db.String(50), nullable=False)
+    origin_url = db.Column(db.String(250), nullable=True)
+
+    tags = db.Column(db.Integer(), ForeignKey('tag.id'), nullable=True)
+    tag = relationship("Tags")
 
     def __repr__(self):
         if self.description:
             self.description = "{0}...".format(self.description[0:10])
 
-        return ("TVSerie(id={!r}, name={!r}, "
-                "description={!r}, episodies_number={!r})").format(
-            self.id, self.name,
+        return ("Responses(id={!r}, end_user_id={!r},  score={!r}, "
+                "description={!r}, campaign_id={!r}), "
+                "created_at={!r}, updated_at={!r}), "
+                "ip_address={!r}, origin_url={!r}),  tags={!r})").format(
+            self.id,
+            self.end_user_id,
+            self.score,
             self.description,
-            self.episodies_number)
+            self.campaign_id,
+            self.created_at,
+            self.updated_at,
+            self.ip_address,
+            self.origin_url,
+            self.tags)
+
+
+class Tags(db.Model):
+    id = db.Column(db.Integer(),
+                   nullable=False, unique=True,
+                   autoincrement=True, primary_key=True)
+    name = db.Column('name', db.String(200), nullable=False)
+
+    def __repr__(self):
+        return ("Tags(id={!r}, name={!r}").format(
+            self.id,
+            self.name)
